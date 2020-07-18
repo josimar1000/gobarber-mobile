c3ffb81d4239135ba80a8817a91c7def63ff1126
Josimar Lima <osimar.lima2007@gmail.com>
2020-07-18 19:43:46 -0300

    fix android errors

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="312e38" />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{ flex: 1, backgroundColor: '#312e38' }}>
       <Routes />
     </View>
